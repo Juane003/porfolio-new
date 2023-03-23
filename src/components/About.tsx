119439f3910b45b870856cf4bfeb3fefcdbdfe70
Juane003 <j.emilio164@gmail.com>
2023-03-23 14:40:39 -0300

    Added CV download link

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -26,7 +26,8 @@ export const About = () => {
         </a>
 
         <a
-          href="#contact"
+          href="CV-Juan-Emilio-Rivero..pdf"
+          download
           className="flex items-center justify-center bg-zinc-200 w-32 h-8 text-zinc-900 p-2 rounded-md hover:bg-transparent hover:text-zinc-200 border hover:border-zinc-200 duration-300 font-semibold"
         >
           Download CV
